e58791bdaaa4e38a613d400452fa410ca9fbdb8e
Unknown <petar.v.todorov@gmail.com>
2018-07-25 22:08:42 -0400

    update process_data_genes.sh
    - forgot python call before one of the scripts... now works

--- process_data/process_data_genes.sh
+++ process_data/process_data_genes.sh
@@ -5,4 +5,4 @@ python process_data_genes_02.py
 python process_data_genes_03.py
 python process_data_genes_04.py
 python process_data_genes_05.py
-process_data_genes_AZ_00.py
+python process_data_genes_AZ_00.py
