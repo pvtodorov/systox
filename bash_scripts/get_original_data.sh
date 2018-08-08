732b5b6179335c13778e34bb7bfc4e6f15e9478d
Unknown <petar.v.todorov@gmail.com>
2018-08-07 23:33:35 -0400

    fetch lists recursively

--- bash_scripts/get_original_data.sh
+++ bash_scripts/get_original_data.sh
@@ -8,7 +8,7 @@ synapse get syn11697966 -r --downloadLocation data/original_data/.
 echo "Make data/lists/ directory."
 mkdir data/lists
 echo "Fetch lists from Synapse."
-synapse get syn13922909 --downloadLocation data/lists/.
+synapse get syn13922909 -r --downloadLocation data/lists/.
 echo "Deleting Synapse manifest .tsv files."
 for i in `find . -name "*.tsv" -type f`; do
     rm "$i"
